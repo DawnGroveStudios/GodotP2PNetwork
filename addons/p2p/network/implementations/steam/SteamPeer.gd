de1036e675ded9545cf6fe470c9fd3e25c0d16ca
Seann-Moser <seannmoser@gmail.com>
2023-09-28 16:39:53 -0700

    fix: minor issue when getting steam profile name

--- addons/p2p/network/implementations/steam/SteamPeer.gd
+++ addons/p2p/network/implementations/steam/SteamPeer.gd
@@ -2,13 +2,18 @@ extends NetPeer
 
 class_name SteamPeer
 var SteamClient
+
 func _init(network_id:int,data:PackedByteArray=PackedByteArray()) -> void:
 	super(network_id,data)
 	if Engine.has_singleton("Steam"):
 		SteamClient = Engine.get_singleton("Steam")
 	else:
 		return
+
 func get_profile_name() -> String:
 	if self.profile_name == "" || profile_name == null:
-		self.profile_name = SteamClient.getFriendPersonaName(network_id)
+		if SteamClient == null and Engine.has_singleton("Steam"):
+			SteamClient = Engine.get_singleton("Steam")
+		if SteamClient != null:
+			self.profile_name = SteamClient.getFriendPersonaName(network_id)
 	return profile_name
